fd181b0f1295aa5a69a3676df3beaafb319746cc
lennart@familie-richter.de <lennart@familie-richter.de>
2023-04-03 20:05:30 -0700

    Finished the selection sort

--- LinkedList/Source.cpp
+++ LinkedList/Source.cpp
@@ -25,7 +25,7 @@ LinkedListNode* GenLinked()
 	LinkedListNode* current = nullptr;
 	LinkedListNode* first = nullptr;
 
-	for (int i = 0; i < 5; ++i)
+	for (int i = 0; i < 10; ++i)
 	{
 		string current_val;
 		cin >> current_val;
@@ -86,20 +86,92 @@ LinkedListNode* BinarySearch(LinkedListNode* start, LinkedListNode* end, string 
 	LinkedListNode* endPt = end;
 	LinkedListNode* midpoint = FindMid(start, end);
 
-	if (midpoint->val < start->val) {
-		return nullptr;
-	}
 	if (midpoint->val == val) {
 		return midpoint;
 	}
+	if (midpoint->val == start->val) {
+		return nullptr;
+	}
 	else if (midpoint->val < val) {
-		return BinarySearch(midpoint, end, val);
+		return BinarySearch(midpoint->next, end, val);
 	}
 	else if (midpoint->val > val) {
 		return BinarySearch(start, midpoint->prev, val);
 	}
+}
 
+LinkedListNode* swapNodes(LinkedListNode* node1, LinkedListNode* node2) {
+	if (node1->next == node2) {
+		LinkedListNode* tempBack = node1->prev;
+		LinkedListNode* tempFront = node1->next;
+		node1->prev = node2;
+		node1->next = node2->next;
+		if (node2->next != nullptr) {
+			node2->next->prev = node1;
+		}
+		node2->prev = tempBack;
+		if (tempBack != nullptr) {
+			tempBack->next = node2;
+		}
+		node2->next = node1;
+		return node2;
+	}
+	else {
+		LinkedListNode* tempBack = node1->prev;
+		LinkedListNode* tempFront = node1->next;
+		node1->prev = node2->prev;
+		if (node2->prev != nullptr) {
+			node2->prev->next = node1;
+		}
+		node1->next = node2->next;
+		if (node2->next != nullptr) {
+			node2->next->prev = node1;
+		}
+		node2->prev = tempBack;
+		if (tempBack != nullptr) {
+			tempBack->next = node2;
+		}
+		node2->next = tempFront;
+		if (tempFront != nullptr) {
+			tempFront->prev = node2;
+		}
+		return node2;
+	}
+}
 
+LinkedListNode* findMin(LinkedListNode* start) {
+	LinkedListNode* minNode = start;
+	LinkedListNode* traverser = start;
+	while (traverser != nullptr) {
+		if (traverser->val < minNode->val) {
+			minNode = traverser;
+		}
+		traverser = traverser->next;
+	}
+	return minNode;
+}
+
+LinkedListNode* SelectionSort(LinkedListNode* head) {
+	LinkedListNode* sortedHead = head;
+	LinkedListNode* sortedTail = head;
+
+	if (findMin(sortedTail)->val < sortedHead->val) {
+		sortedHead = swapNodes(sortedTail, findMin(sortedTail));
+	}
+
+	sortedTail = sortedHead->next;
+
+	while (sortedTail != nullptr) {
+		PrintLinked(sortedTail);
+		cout << endl;
+		if (findMin(sortedTail)->val < sortedTail->val) {
+			sortedTail = swapNodes(sortedTail, findMin(sortedTail));
+		}
+		sortedTail = sortedTail->next;
+	}
+
+	//cout << sortedHead->val << endl;
+	return sortedHead;
 }
 
 
@@ -107,14 +179,22 @@ int main()
 {
 	LinkedListNode* linked = GenLinked();
 	cout << "Printing Linked List: " << endl;
-	PrintLinked(linked);
+	//PrintLinked(linked);
 
 	//SearchLinked(linked, 0);
 	//SearchLinked(linked, 1);
 	//SearchLinked(linked, 9);
 	//SearchLinked(linked, 10);
 
-	cout << BinarySearch(linked, nullptr, "f")->val;
+	/*if (BinarySearch(linked, nullptr, "f") != nullptr) {
+		cout << "Value Found";
+	}
+	else {
+		cout << "Null";
+	}*/
+
+	PrintLinked(SelectionSort(linked));
+	//PrintLinked(linked);
 
 
 	while (linked->next != nullptr)
